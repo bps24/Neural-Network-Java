dc1dbb32ddbd6bc9c8ffe5905dc22b6c6039a1d6
bs482 <bs482@pvsd.org>
2019-01-29 11:02:24 -0500

    idk i started it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
+import java.util.*;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	    Scanner k = new Scanner(System.in);
+        System.out.println("Enter a RGB Triplet");
+
+        int [] dec = new int [3];
+
+
+        for(int i=0;i<=2;i++)
+        {
+            dec[i]=Integer.parseInt(k.next());
+        }
+
+        System.out.println(Main.check(Main.dectohex(dec)));
     }
+
+
+    public static String dectohex(int [] a)
+    {
+       a[0]/16
+    }
+
+    public static String check(String a)
+    {
+
+    }
+
 }
